8bf86833090d84692492047158b3a632208f4689
Sufff07 <105107281+Sufff07@users.noreply.github.com>
2025-04-16 21:23:09 +0530

    Create AddTwoNumbers.java

---
+++ AddTwoNumbers.java
@@ -0,0 +1,13 @@
+public class AddTwoNumbers {
+    public static void main(String[] args) {
+        // Declare two numbers
+        int num1 = 5;
+        int num2 = 10;
+
+        // Add the two numbers
+        int sum = num1 + num2;
+
+        // Print the result
+        System.out.println("The sum of " + num1 + " and " + num2 + " is: " + sum);
+    }
+}
